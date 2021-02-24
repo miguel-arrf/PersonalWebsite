cbd03c1a683afe34f811ad8ef89b5c022a4e2c0d
miguel-arrf <mferreira.oeiras@gmail.com>
2021-02-24 23:16:25 +0000

    First page url fixed

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('main/', views.main, name='main'),
+    path('', views.main, name='main'),
 ]
