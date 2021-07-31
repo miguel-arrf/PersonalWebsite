c3031f5b066e097765f47dd670a5c1fff9f53ff7
miguel-arrf <mferreira.oeiras@gmail.com>
2021-07-31 18:15:10 +0100

    Ready for production!

--- personalWebsite/settings.py
+++ personalWebsite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_j)-)#qnbke3jf2c#=czz$y^smj(3dg*y)_6u!#1!znsxeg*9#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['personal-website-miguel-arrf.herokuapp.com']
 
